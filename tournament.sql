f7f9523aab58e1ee694a279783a85244869ac1cf
gbedell <gavinbedell@Gavins-MBP.home>
2015-12-13 09:35:44 -0500

    Create standings view table

--- tournament.sql
+++ tournament.sql
@@ -27,3 +27,47 @@ CREATE TABLE match (
 	winner_id integer REFERENCES player (user_id),
 	loser_id integer REFERENCES player (user_id)
 );
+
+CREATE VIEW standings AS 
+SELECT
+wins_table.id as id,
+wins_table.name as name,
+wins_table.wins as wins,
+wins_table.wins + losses_table.losses as matches
+
+FROM
+
+(SELECT
+player.user_id as id,
+player.name as name,
+count(match.winner_id) as wins
+
+FROM
+player
+left join match
+on player.user_id = match.winner_id
+
+GROUP BY        
+player.user_id)
+AS wins_table
+
+LEFT JOIN    
+
+(SELECT
+player.user_id as id,
+player.name as name,
+count(match.loser_id) as losses                
+
+FROM
+player
+left join match
+on player.user_id = match.loser_id
+
+GROUP BY
+player.user_id)
+AS losses_table
+
+ON wins_table.id = losses_table.id
+
+ORDER BY
+wins_table.wins desc
